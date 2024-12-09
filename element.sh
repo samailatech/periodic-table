8622c9fbc5c0f2b58c4ea44594132b51f045baab
samailatech samaila <samailasamailatech@gmail.com>
2024-12-09 00:28:21 +0000

    feat: Add element.sh script for element lookup

--- element.sh
+++ element.sh
@@ -35,3 +35,4 @@ else
     echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
   done
 fi
+#dksdkdvms;lvdmdl
